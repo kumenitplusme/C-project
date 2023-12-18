225f8113858346029a2ea70aac700c1c56a5ba0b
natisis94 <125308173+kumenitplusme@users.noreply.github.com>
2023-12-18 15:45:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () => {
+    profile.classList.toggle('active');
+}
+window.onscroll = () => {
+    profile.classList.remove('active');
+}
